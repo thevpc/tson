03ddea307825a98ce7d7ee1d71ba4012e72e59fa
thevpc <taha.bensalah@gmail.com>
2024-09-14 17:46:42 +0100

    [UPDATED] 20240914

---
+++ test/tson-test/src/main/java/net/thevpc/tson/TestParser1.java
@@ -0,0 +1,29 @@
+package net.thevpc.tson;
+
+public class TestParser1 {
+    public static void main(String[] args) {
+        a01();
+//        a02();
+    }
+    public static void a01() {
+        String text="-1";
+        TsonDocument e = Tson.reader().readDocument(text);
+        System.out.println(e);
+    }
+    public static void a02() {
+        String text="switch(){\n" +
+                "    case \"\":{}\n" +
+                "    case \"\":{}\n" +
+                "}";
+        TsonDocument e = Tson.reader().readDocument(text);
+        System.out.println(e);
+    }
+    public static void a03() {
+        String text="switch(a+2-3){\n" +
+                "    case \"\"=>{}\n" +
+                "    case \"\"=>{}\n" +
+                "}";
+        TsonDocument e = Tson.reader().readDocument(text);
+        System.out.println(e);
+    }
+}
